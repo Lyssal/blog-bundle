11989f12d2b014a289b37f15a116c3ae7de99031
Lyssal <locelot@lyssal.com>
2018-11-01 16:08:43 +0100

    Category manager : Add getByParent method

--- src/Manager/CategoryManager.php
+++ src/Manager/CategoryManager.php
@@ -7,12 +7,30 @@
  */
 namespace Lyssal\BlogBundle\Manager;
 
+use Lyssal\BlogBundle\Entity\Category;
 use Lyssal\Doctrine\Orm\Manager\EntityManager;
+use Lyssal\Doctrine\Orm\QueryBuilder;
 
 /**
  * @inheritDoc
  */
 class CategoryManager extends EntityManager
 {
+    /**
+     * Get the categories by parent.
+     *
+     * @param \Lyssal\BlogBundle\Entity\Category|null $categoryParent The parent category
+     *
+     * @return Lyssal\BlogBundle\Entity\Category[] The sub categories
+     */
+    public function getByParent(?Category $categoryParent = null): array
+    {
+        if (null === $categoryParent) {
+            $conditions = [QueryBuilder::WHERE_NULL => 'parent'];
+        } else {
+            $conditions = ['parent' => $categoryParent];
+        }
 
+        return $this->findBy($conditions);
+    }
 }
